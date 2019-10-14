33caa2d0e98045fedfb19f6f3150d6555059750d
Victor Rono <v.rono@icloud.com>
2019-10-14 14:58:15 -0500

    displayBooks method made static, didn't want to instantiate it. Hard coded array of books for testing

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ class Book{
 	}
 }
 //UI Class: Handle UI tasks
+class UI {
+	static displayBooks(){
+		const StoredBooks = [
+          {
+          	title: 'Book One',
+          	author: 'John Doe',
+          	isbn: '3434434'
+          },
+          {
+          	title: 'Book Two',
+          	author: 'Jane Doe',
+          	isbn: '45545'
+          }
+		];
+	}
+}
 //Store Class:Handles strorage
 //Event: Display Book
 //Event: Add a book
