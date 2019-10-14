763d410a8ea08a1ce6ff5ffeb8d294781bce2717
Victor Rono <v.rono@icloud.com>
2019-10-14 14:42:14 -0500

    correction-book class now added

---
+++ app.js
@@ -0,0 +1,13 @@
+//Book class: Represents a book
+class Book{
+	constructor(title, author, isbn){
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+	}
+}
+//UI Class: Handle UI tasks
+//Store Class:Handles strorage
+//Event: Display Book
+//Event: Add a book
+//Event: Remove a book
